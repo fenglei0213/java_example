e62aecb32f020c1993e1fd903df8846e42648521
fenglei <fenglei0213@163.com>
2016-08-27 10:05:01 +0800

    m

--- src/main/java/org/wind3stone/example/java/datatype/ObjectType.java
+++ src/main/java/org/wind3stone/example/java/datatype/ObjectType.java
@@ -1,7 +1,34 @@
 package org.wind3stone.example.java.datatype;
 
+import org.wind3stone.example.java.vo.Apple;
+
 /**
  * Created by fenglei on 2016/7/27.
  */
 public class ObjectType {
+
+    public static void main(String[] args) {
+        Apple apple = ObjectType.getAppleObject();
+        System.out.println(apple.hashCode());
+    }
+
+    public static Apple getAppleObject() {
+        Apple apple = new Apple();
+        apple.setName("abc");
+        return apple;
+    }
+
+    public static void doHashCode() {
+
+    }
+
+//    @Override
+//    public int hashCode() {
+//        return 0;
+//    }
+
+//    public void hashCode(){
+//
+//    }
+
 }
---
+++ src/main/java/org/wind3stone/example/java/vo/Apple.java
@@ -0,0 +1,17 @@
+package org.wind3stone.example.java.vo;
+
+/**
+ * Created by fenglei on 2016/8/27.
+ */
+public class Apple {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
